90e5708c0ab557d2823f573226e0757397a66393
Kuzmenko Alexandr <akuzmenko@vodafone.ua>
2019-07-15 10:15:50 +0300

    first commit

---
+++ src/NextValue.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NextValue {
+
+    public synchronized int getNextValue() {
+        return value++;
+    }
+
+    public int getNextValue() {
+        synchronized (this) {
+            return value++;
+        }
+    }
+
+    public int getNextValue() {
+        return new AtomicInteger(value).incrementAndGet();
+    }
+}
